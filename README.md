368cc77bddf29699de5b375ede14f6bc1dc590dd
Shahdab Aalam Saifi <saalamsaifi@gmail.com>
2019-10-05 13:46:13 +0530

    FilterEvenOddApplication

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Playground
+
+* Filtering even and odd numbers
---
+++ src/com/saalamsaifi/evenodd/FilterEvenOdd.java
@@ -0,0 +1,47 @@
+package com.saalamsaifi.evenodd;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class FilterEvenOdd {
+	
+	private boolean isEven(int number) {
+		return number % 2 == 0;
+	}
+
+	public int[] randomIntegers(int length) {
+		if (length <= 0) {
+			return new int[] {};
+		}
+
+		Random random = new Random(System.currentTimeMillis());
+		int[] array = new int[length];
+
+		for (int i = 0; i < length; i++) {
+			array[i] = random.nextInt(1000);
+		}
+
+		return array;
+	}
+
+	// TODO: Avoid/remove this warning
+	@SuppressWarnings("unchecked")
+	public ArrayList<ArrayList<Integer>>[] filterEvenOdd(int[] numbers) {
+		if (numbers == null) {
+			return new ArrayList[] {}; 
+		}
+
+		ArrayList<Integer> evenNumbers = new ArrayList<>();
+		ArrayList<Integer> oddNumbers = new ArrayList<>();
+
+		for (Integer integer : numbers) {
+			if (isEven(integer)) {
+				evenNumbers.add(integer);
+			} else {
+				oddNumbers.add(integer);
+			}
+		}
+
+		return new ArrayList[] { evenNumbers, oddNumbers };
+	}
+}
