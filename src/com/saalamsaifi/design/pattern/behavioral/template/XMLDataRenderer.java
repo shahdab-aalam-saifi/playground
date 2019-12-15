f2d199d5e851d5e843d14b0a7f2a27ee48d22f3b
Shahdab Aalam Saifi <saalamsaifi@gmail.com>
2019-12-15 22:17:14 +0530

    Template Method

---
+++ src/com/saalamsaifi/design/pattern/behavioral/template/XMLDataRenderer.java
@@ -0,0 +1,15 @@
+package com.saalamsaifi.design.pattern.behavioral.template;
+
+public class XMLDataRenderer extends DataRenderer {
+
+	@Override
+	public String read() {
+		return "<xml>data</xml>";
+	}
+
+	@Override
+	public String process(String data) {
+		return "<xml><processed>" + data + "</processed></xml>";
+	}
+
+}
