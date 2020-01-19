d373013ec7328fb1db38abdc8cace7c63dd6aa39
Shahdab Aalam Saifi <saalamsaifi@gmail.com>
2020-01-19 13:10:40 +0530

    Immutable class with mutable object reference

--- src/com/saalamsaifi/immutable/ImmutableApplication.java
+++ src/com/saalamsaifi/immutable/ImmutableApplication.java
@@ -1,13 +1,15 @@
 package com.saalamsaifi.immutable;
 
 public class ImmutableApplication {
-	public static void main(String[] args) {
-		Student student = new Student(100, "Shahdab");
-		
-		System.out.println(student.getRollNumber() + " " + student.getName());
-		
-		student = new Student(100, "Shahdab Aalam");
+	public static void main(String[] args) throws CloneNotSupportedException {
+		Course msc = new Course(01L, "M. Sc. Computer Science", 3);
+		Student student = new Student(100, "Shahdab", msc);
 
-		System.out.println(student.getRollNumber() + " " + student.getName());
+		System.out.println(student.getCourse());
+
+		Course modifiedCourse = student.getCourse();
+		modifiedCourse.setId(101L); // performing changes on deep copy of object
+
+		System.out.println(student.getCourse()); // change MUST not be applied on this reference
 	}
 }
